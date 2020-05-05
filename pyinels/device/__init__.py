b578c4bc233e5e23362841e6d00aeebbcbe88b3a
Jiri Horalek <horalek.jiri@gmail.com>
2020-05-05 23:30:05 +0200

    - remove switch control. turn on/off in pyInels class. - remove device tests

--- pyinels/device/__init__.py
+++ pyinels/device/__init__.py
@@ -1,30 +1,8 @@
 """Common device class for iNels BUS."""
-import logging
-
 from pyinels.api.resources import ApiResource
-from pyinels.const import (
-    ATTR_SWITCH
-)
-
-from pyinels.device.control.switch import SwitchControl
-
-_LOGGER = logging.getLogger(__name__)
 
 
 class Device(ApiResource):
-
-    @property
-    def has_switch_control(self):
-        """Determine if this device has switch_control."""
-        return self.type == ATTR_SWITCH
-
-    @property
-    def switch_control(self):
-        if self.has_switch_control:
-            return SwitchControl(self)
-
-        return None
-
     def __repr__(self):
         """Object representation."""
         return "<{} - {} type({})>".format(self.id, self.title, self.type)
